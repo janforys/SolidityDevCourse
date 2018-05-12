f3db762b38435978141b4f2439607cb29ba516a1
Jan <janforys@wp.pl>
2018-05-12 15:53:00 +0200

    Async/Await Refactor

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,15 +3,15 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+beforeEach(async () => {
 // Get a list of all accounts
-web3.eth.getAccounts().then(fetchedAccounts => {
-    console.log(fetchedAccounts)
-});
+accounts = await web3.eth.getAccounts();
 
 // Use one of those accounts to deploy a contract
 });
 
 describe('Inbox',() => {
-it('deploys a contract',() => {});
+    it('deploys a contract',() => {
+        console.log(accounts);
+    });
 });
