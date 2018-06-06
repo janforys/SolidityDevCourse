3b8605992779bfa167a2748b1f8c2dbfbb73eca9
Jan <janforys@wp.pl>
2018-06-07 00:05:51 +0200

    Creating Smart Contract of ERC-20 Token

--- token_sale/contracts/DappToken.sol
+++ token_sale/contracts/DappToken.sol
@@ -1,10 +1,17 @@
 pragma solidity ^0.4.2;
 
 contract DappToken {
+
     uint256 public totalSupply;
+    string public name = "DApp Token";
+    string public standard = "DApp Token v1.0";
+    string public symbol = "DApp";
+
+    mapping(address => uint256) public balanceOf;
     // Constructor
-    constructor() public {
-        totalSupply = 1000000;
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
     // Set total number of tokens
     // Read total number of tokens
--- token_sale/test/DappToken.js
+++ token_sale/test/DappToken.js
@@ -1,13 +1,32 @@
 var DappToken = artifacts.require("./DappToken.sol");
 
 contract('DappToken', function(accounts) {
+    var tokenInstance;
 
-    it('Sets the total supply upon deployment', function() {
+    it('Initializes the contract with the correct values', function()  {
+        return DappToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name) {
+            assert.equal(name, 'DApp Token', 'has the correct name');
+            return tokenInstance.symbol();
+        }).then(function(symbol) {
+            assert.equal(symbol, 'DAPP', 'has the correct symbol');
+            return tokenInstance.standard();
+        }).then(function(standard) {
+            assert.equal(standard, 'DApp Token v1.0', 'has the correct standard');
+        });
+    })
+
+    it('Allocates initial supply upon deployment', function() {
         return DappToken.deployed().then(function(instance) {
             tokenInstance = instance;
             return tokenInstance.totalSupply();
         }).then(function(totalSupply) {
             assert.equal(totalSupply.toNumber(), 1000000, 'Sets the total supply to 1,000,000');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(adminBalance) {
+            assert.equal(adminBalance.toNumber(), 1000000, 'It allocates inital supply to the admin');
         });
     });
-})
+});
