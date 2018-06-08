27a3f8476d62810d864d2fe7b0bdb04b29e33752
Jan <janforys@wp.pl>
2018-06-08 10:17:34 +0200

    Transfer Tokens

--- token_sale/contracts/DappToken.sol
+++ token_sale/contracts/DappToken.sol
@@ -7,12 +7,26 @@ contract DappToken {
     string public standard = "DApp Token v1.0";
     string public symbol = "DApp";
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
     // Constructor
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
-    // Set total number of tokens
-    // Read total number of tokens
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
 }
